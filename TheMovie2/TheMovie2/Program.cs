f298d9bc7b98f681c5d60ab1a8c76292fb0ada1c
Blake <bruseloff.blake@gmail.com>
2020-11-11 18:40:44 -0500

    update

---
+++ TheMovie2/TheMovie2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliverable11
+{
+    class MainClass
+    {
+        private static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Movie List Appliaction!");
+            Console.WriteLine("There are 10 Movies in this list.");
+
+            Console.WriteLine("What category are you interested in: Enter Number 1-4?");
+            Console.WriteLine("Horror");
+            Console.WriteLine("Drama");
+            Console.WriteLine("Sci-Fi");
+            Console.WriteLine("Animation");
+
+            string mySearchString = Console.ReadLine();
+
+
+
+
+
+            List<Movie> allMovies = new List<Movie>();
+
+
+
+            Movie sawOne = new Movie("Saw 1", "Horror");
+            Movie sawTwo = new Movie("Saw 2", "Horror");
+            Movie theAvengers = new Movie("Avengers", "Drama");
+            Movie theFight = new Movie("Fight Club", "Drama");
+            Movie theLooper = new Movie("Looper", "Sci-Fi");
+            Movie theVR = new Movie(" Ready Player One", "Sci-Fi");
+            Movie theTS = new Movie("Toy Story 1", "Animation");
+            Movie theTS2 = new Movie("Toy Story 2", "Animation");
+            Movie theTS3 = new Movie("Toy Story 3", "Animation");
+            Movie theTS4 = new Movie("Toy Story 4", "Animation");
+
+            allMovies.Add(sawOne);
+            allMovies.Add(sawTwo);
+            allMovies.Add(theAvengers);
+            allMovies.Add(theFight);
+            allMovies.Add(theLooper);
+            allMovies.Add(theVR);
+            allMovies.Add(theTS);
+            allMovies.Add(theTS2);
+            allMovies.Add(theTS3);
+            allMovies.Add(theTS4);
+
+
+            
+
+            foreach (Movie currentMovie in allMovies)
+            {
+                if (currentMovie.getCategory() == mySearchString)
+                {
+                    Console.WriteLine(currentMovie.getTitle());
+                }
+
+               
+
+            }
+
+        }
+
+       
+
+
+
+
+        public class Movie
+        {
+
+           
+
+
+            public Movie(string title, string category)
+            {
+                _title = title;
+                _category = category;
+            }
+
+            private string _title;
+            private string _category;
+
+            public string getTitle()
+            {
+                return _title;
+            }
+            public string getCategory()
+            {
+                return _category;
+            }
+
+           
+        }
+
+
+    }
+}
